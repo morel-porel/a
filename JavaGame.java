20a77ccfa58ef93989656c574836c232a71e2184
morel-porel <132826194+morel-porel@users.noreply.github.com>
2024-10-29 20:56:54 +0800

    Update JavaGame.java

--- JavaGame.java
+++ JavaGame.java
@@ -15,6 +15,16 @@ public class JavaGame {
      */
     public static void main(String[] args) {
         // Define characters
+        /*
+        System.out.println(" _____     _                              __   _     _       _     _   ");
+        System.out.println("|  ___|   | |                            / _| | |   (_)     | |   | |  ");
+        System.out.println("| |__  ___| |__   ___   ___  ___    ___ | |_  | |    _  __ _| |__ | |_ ");
+        System.out.println("|  __|/ __| '_ \\ / _ \\ / _ \\/ __|  / _ \\|  _| | |   | |/ _` | '_ \\| __|");
+        System.out.println("| |__| (__| | | | (_) |  __/\\__ \\ | (_) | |   | |___| | (_| | | | | |_ ");
+        System.out.println("\\____/\\___|_| |_|\\___/ \\___||___/  \\___/|_|   \\_____/_|\\__, |_| |_|\\__|");
+        System.out.println("                                                        __/ |          ");
+        System.out.println("                                                       |___/           ");
+        */
         CharacterJascha Jascha = new CharacterJascha(150, 50);
         CharacterElara Elara = new CharacterElara(150, 50);
         CharacterFinn Finn = new CharacterFinn(150, 50);
