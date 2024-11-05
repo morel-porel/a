0e9baab1adab590bf12b998be11366e8ecb36463
jskakk <143810230+jskakk@users.noreply.github.com>
2024-11-05 23:18:11 +0800

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -9,6 +9,18 @@ package game;
  * @author User
  */
 public class Inventory {
+    
+    public static final String RESET = "\u001B[0m";  // Reset to default
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String MAGENTA = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String ORANGE = "\033[38;5;214m";
+    public static final String RED_BACKGROUND = "\033[41m"; 
+    public static final String BLUE_BACKGROUND = "\033[44m";
+    
     private int healthPotions;
     private int manaPotions;
     private int gold;
@@ -38,7 +50,7 @@ public class Inventory {
             healthPotions--;
             int healAmount = 50;
             target.setHP(target.getHP() + healAmount);  
-            System.out.println(target.displayName()+" used a health potion, restored 50HP.");
+            System.out.println(target.displayName()+ RESET +" used a health potion, restored "+ RED_BACKGROUND + "50HP."+RESET);
         } else {
             System.out.println("No health potions left!");
         }
@@ -58,7 +70,7 @@ public class Inventory {
             manaPotions--;
             int healAmount = 50;
             target.setMP(target.getMP() + healAmount);  
-            System.out.println(target.displayName()+" used a mana potion, restored 50MP.");
+            System.out.println(target.displayName()+RESET+" used a mana potion, restored " + BLUE_BACKGROUND + "50MP." + RESET);
         } else {
             System.out.println("No mana potions left!");
         }
@@ -76,7 +88,7 @@ public class Inventory {
     public void spendGold(int amount) {
         if (gold >= amount) {
             gold -= amount;
-            System.out.println("You spent " + amount + " gold.");
+            System.out.println("You spent "+ YELLOW + amount + " gold.");
         } else {
             System.out.println("Not enough gold!");
         }
@@ -113,8 +125,8 @@ public class Inventory {
     public void displayInventory() {
         System.out.println();
         System.out.println("Inventory:");
-        System.out.println("XP: " + xp + " / "+ xpReq);
-        System.out.println("Gold: " + gold);
+        System.out.println(GREEN +"XP: " + xp + " / "+ xpReq);
+        System.out.println(YELLOW +"Gold: " + gold);
         System.out.println("Health Potions: " + healthPotions);
         System.out.println("Mana Potions: " + manaPotions);
         System.out.println();
