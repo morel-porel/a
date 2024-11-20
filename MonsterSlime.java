ab54a3266e51f9354af2d4e6f25c060d5da935a8
Kate P. <katepotane@gmail.com>
2024-11-20 16:22:10 +0800

    Update MonsterSlime.java

--- MonsterSlime.java
+++ MonsterSlime.java
@@ -1,43 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package game;
-
 import java.util.Random;
-
 /**
  *
  * @author User
  */
-public class MonsterSlime extends Monster {
+public class MonsterSlime extends Monster{
     private int HP;
-
-    public MonsterSlime(int HP) {
-        this.HP = HP;
-        System.out.println("A wild Slime has appeared! Starting HP: " + this.HP);
+    
+    public MonsterSlime (int HP){
+        this.HP = HP;       
     }
-
     @Override
-    public String displayName() {
+    public String displayName(){
         return "Slime";
     }
-
     @Override
-    public void setHP(int HP) {
+    public void setHP(int HP){
         this.HP = HP;
     }
-
     @Override
-    public int getHP() {
+    public int getHP(){
         return HP;
     }
-
     @Override
-    public int skillOne() {
+    public int skillOne(){        
         Random ran = new Random();
         int skillOneDamage = ran.nextInt(6);
-        System.out.println("Slime used slime ball! Dealt " + skillOneDamage + " damage!");
+        System.out.println("Slime used slime ball! Dealt "+skillOneDamage +" damage!");
         return skillOneDamage;
-    }
-
-    public void displayCurrentHP() {
-        System.out.println("Slime's current HP: " + this.HP);
     }
 }
